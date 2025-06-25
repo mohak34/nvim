36a81232e8d9eaab4cf9f6a3f93a3d8ac416e28e
mohak34 <mohaks0408@gmail.com>
2025-06-25 23:40:57 +0530

    oil, supermaven

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional: Add a keymap to open oil
+    keys = {
+      { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+      { "\\", "<CMD>Oil<CR>", desc = "Open oil in current directory" },
+    },
+    -- Optional dependencies
+    dependencies = {
+      { 'echasnovski/mini.icons', opts = {} },
+      -- { 'nvim-tree/nvim-web-devicons', opts = {} },
+    },
+    lazy = false,
+  },
+}
