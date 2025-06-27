e29795d37ca0c26b7a65c5f469f1d3b322e04e67
mohak34 <mohaks0408@gmail.com>
2025-06-27 11:46:18 +0530

    updated oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {default_file_explorer = false, view_options = { show_hidden = true },},
     -- Optional: Add a keymap to open oil
     keys = {
       { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
