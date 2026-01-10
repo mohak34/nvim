30d07c55f01f03885932644a4dfa882d0d332780
mohak34 <mohaks0408@gmail.com>
2026-01-11 01:32:14 +0530

    feat: configure telescope to show hidden files and ignore patterns

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -52,15 +52,31 @@ return {
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules/',
+            '.git/',
+            '__pycache__/',
+            '%.pyc',
+            '.venv/',
+            'venv/',
+            '%.egg%-info/',
+            'dist/',
+            'build/',
+            '%.lock',
+            '%.min%.js',
+            '%.min%.css',
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          },
+          live_grep = {
+            additional_args = { '--hidden', '--no-ignore' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
